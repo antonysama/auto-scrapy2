1314b47c568cf1b584cc460ecb167e47cfb96f4d
antonysama <antonysama@gmail.com>
2020-10-20 21:15:05 -0600

    f1 script

--- scrape/script.sql
+++ scrape/script.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS public.table;
 CREATE TABLE public.table (id SERIAL, date DATE, title varchar (400), source varchar(200), link varchar(300));
 COPY public.table FROM '/var/lib/postgresql/data/scrape/f1.csv' WITH CSV HEADER;
-SELECT title, source FROM public.table WHERE date='2020-10-20';
+SELECT title, source FROM public.table WHERE date='2020-10-21';
